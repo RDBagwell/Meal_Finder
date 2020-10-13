2a57d7b70ee534c8625a70ca9e7eb4c71ece504a
Robert B <robertb@lifesciencepublishers.com>
2020-10-13 12:19:49 -0600

    Add CSS AND JS Funtions

--- js/script.js
+++ js/script.js
@@ -20,17 +20,81 @@ const search = document.getElementById('search'),
                 mealsEl.innerHTML = data.meals.map(meal =>`
                     <div class="meal">
                         <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                     </div>
-                     <div class="meal-info" data-mealID="${meal.idMeal}">
-                        <h3>${meal.strMeal}</h3>
-                     </div>
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
                     `).join('');
                     search.value = '' 
             }
         } else {
-            alert('test');
+            alert('Please Enter A Search');
         }
         
     }
 
+    async function getMealById(mealId){
+        const result = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+        const data = await result.json();
+        const meal = data.meals[0];
+
+        addMealToDOM(meal);
+        console.log(data);
+    }
+
+    function addMealToDOM(meal){
+        const ingredinets = [];
+
+        for(let i = 1; i <= 20; i++){
+            if(meal[`strIngredient${i}`]){
+                ingredinets.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+            } else {
+                break;
+            }
+        }
+        singel_mealEl.innerHTML = `
+            <div class="single-image">
+                <h1>${meal.strMeal}</h1>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="single-meal-info">
+                    ${meal.strCategory ? `<p>Category: ${meal.strCategory}</p>`: ''}
+                    ${meal.strArea? `<p>Area: ${meal.strArea}</p>`: ''}
+                </div>
+                <div class="main">
+                    <h2>Ingredinets</h2>
+                    <ul>
+                        ${ingredinets.map(ing=>`<li>${ing}</li>`).join('')}
+                    </ul>
+                    <p>${meal.strInstructions}</p>
+                </div>
+            </div>
+        `;
+    }
+
+    async function randomMeal() {
+        const result = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+        const data = await result.json();
+        // console.log(data);
+    }
+
+
+
     submit.addEventListener('submit', searchMeal);
+    mealsEl.addEventListener('click', (e)=>{
+        const mealInfo = e.path.find(item=>{
+            if(item.classList){
+                return item.classList.contains('meal-info');
+            } else {
+                return false
+            }
+        });
+
+        if(mealInfo){
+            const mealID = mealInfo.getAttribute('data-mealid');
+            getMealById(mealID);
+        }
+        
+    });
+
+
+    random.addEventListener('click', randomMeal)
